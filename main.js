5a55a88b4402fd396e25cd4bae71f9986b021b1b
htmadx <127308472+htmadx@users.noreply.github.com>
2024-05-27 09:50:23 -0300

    ukuyuui

---
+++ main.js
@@ -0,0 +1,11 @@
+const botoes = document.querySelectorA11(".botao");
+
+for(let i=0;1 <botoes.length;i++){
+      botoes[i].onclick = function(){
+       
+        for (let j = 0;j<botoes.length; j++){
+        botoes[j].classlist.remove("ativo");
+        }
+        botoes[i].classlist.add("ativo");
+      }
+}
